ff99bab0c60830cf949f3d70f1a67ab6b65b8055
Dmytro Zemnitskyi <zemnitskiy@gmail.com>
2021-01-17 17:06:09 +0200

    Section First Screen Complete

--- js/main.js
+++ js/main.js
@@ -5,29 +5,23 @@ $(document).ready(function () {
   });
 
   $(".header-burger-close").click(function (event) {
-    $(".header-wrapper, .header-nav").removeClass("active");
-    $(".header-burger").removeClass("header-burger-hide");
+    $(".header-nav").removeClass("active");
+
+    setTimeout(function () {
+      $(".header-wrapper").removeClass("active");
+      $(".header-burger").removeClass("header-burger-hide");
+    }, 300);
   });
 
-  $(".owl-carousel").owlCarousel({
-    responsive: {
-      0: {
-        items: 1,
-        dots: false,
-      },
-      485: {
-        items: 2,
-        dots: false,
-      },
-      728: {
-        items: 2,
-        dots: false,
-      },
-      960: {
-        items: 2,
-        dots: false,
-      },
-    },
-    loop: true,
+  const prevBtn = '<img src="img/arrow-prev.png" /><span>пред</span>';
+  const nextBtn = '<span>след</span><img src="img/arrow-next.png" />';
+
+  $(".owl-poster").owlCarousel({
+    items: 1,
+    autoWidth: true,
+    margin: 21,
+    nav: true,
+    dots: false,
+    navText: [prevBtn, nextBtn],
   });
 });
